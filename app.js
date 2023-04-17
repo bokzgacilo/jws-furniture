d13d3badd1983443dab1de71ec3cc342a1b9a549
Ariel Jericko Gacilo <42305618+bokzgacilo@users.noreply.github.com>
2023-04-17 12:28:09 +0800

    facebook login fix

--- app.js
+++ app.js
@@ -45,45 +45,46 @@ googleButton.addEventListener("click", () => {
     })
 });
 
-// facebookButton.addEventListener("click", () => {
-//   $.ajax({
-//     type: 'get',
-//     url: 'api/facebook-login.php',
-//     success: (response) => {
+facebookButton.addEventListener("click", () => {
+  alert()
+  // $.ajax({
+  //   type: 'get',
+  //   url: 'api/facebook-login.php',
+  //   success: (response) => {
 
-//     }
-//   })
-//   // firebase.auth().signOut()
-//   // .then(function() {
-//   //   console.log("User signed out successfully");
-//   // })
+  //   }
+  // })
+  // firebase.auth().signOut()
+  // .then(function() {
+  //   console.log("User signed out successfully");
+  // })
 
-//   // const provider = new firebase.auth.FacebookAuthProvider();
+  // const provider = new firebase.auth.FacebookAuthProvider();
 
-//   // firebase.auth().signInWithPopup(provider)
-//   // .then((result) => {
-//   //   const user = result.user;
+  // firebase.auth().signInWithPopup(provider)
+  // .then((result) => {
+  //   const user = result.user;
 
-//   //   $.ajax({
-//   //     type: 'post',
-//   //     url: 'api/loginFacebook.php',
-//   //     data: {
-//   //       email: user.email,
-//   //       fullname: user.displayName,
-//   //       photoURL: user.photoURL,
-//   //       uid: user.uid
-//   //     },
-//   //     success: (response) => {
-//   //       if(response == 1){
-//   //         alert('Login from Facebook was successful');
-//   //         localStorage.setItem('authenticated', 'true');
-//   //         localStorage.setItem('uid', user.uid)
-//   //         location.reload();
-//   //       }
-//   //     }
-//   //   })
-//   // })
-// });
+  //   $.ajax({
+  //     type: 'post',
+  //     url: 'api/loginFacebook.php',
+  //     data: {
+  //       email: user.email,
+  //       fullname: user.displayName,
+  //       photoURL: user.photoURL,
+  //       uid: user.uid
+  //     },
+  //     success: (response) => {
+  //       if(response == 1){
+  //         alert('Login from Facebook was successful');
+  //         localStorage.setItem('authenticated', 'true');
+  //         localStorage.setItem('uid', user.uid)
+  //         location.reload();
+  //       }
+  //     }
+  //   })
+  // })
+});
 
 function signout() {
   localStorage.removeItem('authenticated');
