930c556472358e56c004e3d859338306714d3bfc
Ariel Jericko Gacilo <42305618+bokzgacilo@users.noreply.github.com>
2023-04-20 16:59:22 +0800

    fix 04202023

--- config/facebook-config.php
+++ config/facebook-config.php
@@ -2,8 +2,8 @@
   session_start();
   require '../vendor/autoload.php';
 	$fb = new Facebook\Facebook([
-	  'app_id' => '901939544433136', // Replace {app-id} with your app id
-	  'app_secret' => 'b24a86674c7000d632121a01deb1d968', // Replace {app_secret} with your app secret
+	  'app_id' => '778477127194753', // Replace {app-id} with your app id
+	  'app_secret' => '4923a5b47ed7927795dda43b95d82dbd', // Replace {app_secret} with your app secret
 	  'default_graph_version' => 'v2.10',
 	]);
 ?>  
