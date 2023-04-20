767ce1234e620c5484cccf910d259adbea6353f7
Ariel Jericko Gacilo <42305618+bokzgacilo@users.noreply.github.com>
2023-04-20 16:49:43 +0800

    login redirect url fix

--- api/facebook-login.php
+++ api/facebook-login.php
@@ -1,6 +1,6 @@
 <?php 
 	require '../config/facebook-config.php';
 	$helper = $fb -> getRedirectLoginHelper();
-	$loginUrl = $helper -> getLoginUrl('http://localhost/jws-furniture/api/facebook-callback.php');
+	$loginUrl = $helper -> getLoginUrl('https://jwsfurniture.website/api/facebook-callback.php');
 	header("location:" . $loginUrl);
 ?>
